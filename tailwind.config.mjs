7f769851da99ca876368fae26b97add895a8c96d
Chein <devops@bigbige.com>
2024-05-19 14:55:46 +0800

    😘 feat: support anchor and use prism to highlight pre/code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -37,16 +37,44 @@ export default {
 
 		}
 	},
-	plugins: [require("daisyui")],
 	daisyui: {
+		themes: [
+			{
+				light: {
+					"primary": "#89e1a8",
+					"secondary": "#603f64",
+					"accent": "#fa8900",
+					"neutral": "#3fbbbb",
+					"base-100": "#ffffff",
+					"info": "#d8ff9d",
+					"success": "#62a300",
+					"warning": "#ffe66b",
+					"error": "#ff886b",
+				},
+				dark: {
+					"primary": "#89e1a8",
+					"secondary": "#603f64",
+					"accent": "#fa8900",
+					"neutral": "#3fbbbb",
+					"base-100": "#222222",
+					"info": "#d8ff9d",
+					"success": "#62a300",
+					"warning": "#ffe66b",
+					"error": "#ff886b",
+				}
+			}
+		],
 		// themes: ["light", "black", "luxury"],
-		themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
+		// themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+		// darkTheme: "dark", // name of one of the included themes for dark mode
 		base: true, // applies background color and foreground color for root element by default
-		styled: true, // include daisyUI colors and design decisions for all components
-		utils: true, // adds responsive and modifier utility classes
-		prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-		themeRoot: ":root",
-	}
+		// styled: true, // include daisyUI colors and design decisions for all components
+		// utils: true, // adds responsive and modifier utility classes
+		// prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+		// logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+		// themeRoot: ":root",
+	},
+
+	plugins: [require("daisyui")],
+
 }
