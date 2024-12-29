989113650a4565a7a10f64732cb49c95d52e946f
Chein <devops@bigbige.com>
2024-12-29 15:09:37 +0800

    💦 chores: ui changes

--- public/scripts/motion/scroll.js
+++ public/scripts/motion/scroll.js
@@ -11,3 +11,27 @@ scroll(({ y }) => {
     progress.innerHTML = y.progress.toFixed(2)
 });
 
+// // Pass an animation or timeline to automatically scrub
+// scroll(
+//     animate(".progress-bar", { scaleX: [0, 1] })
+// );
+
+// // Or pass a function to directly use the scroll info
+// // const progress = document.querySelector(".progress")
+// // scroll(({ y }) => {
+// //     progress.innerHTML = y.progress.toFixed(2)
+// // });
+
+/**
+ * In this demo, the circular progress indicator will start
+ * when an <article /> element starts to leave the top of
+ * the viewport and complete when it enters the bottom of the viewport.
+ */
+
+// scroll(animate(".progress-bar", { strokeDasharray: ["0,1", "1,1"] }), {
+//     target: document.getElementById("a-article")
+// });
+//
+// scroll(animate(".progress-bar", { strokeDasharray: ["0,1", "1,1"] }), {
+//     target: document.getElementById("b-article")
+// });
